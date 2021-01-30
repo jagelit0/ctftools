6903d6963373dc75add77dd322166e7be949a741
jagelit0 <73878309+jagelit0@users.noreply.github.com>
2021-01-30 03:08:44 +0100

    Update ctftool.sh

--- ctftool.sh
+++ ctftool.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+YELLOW='\e[1;33m'
 PURPLE='\e[1;35m'
 RED='\e[1;31m'
 CYAN='\e[1;32m'
@@ -15,39 +16,61 @@ read -p "Select where you want to install the script: " dirdown
 if [ -z $dirdown ]; then
          echo -e "${RED}[!] Specify a directory, please [!] ${END}"
 else
-         mkdir -p $dirdown/
-         mkdir -p $dirdown/Tools
-         mkdir -p $dirdown/Wordlists
+	mkdir $dirdown/
+	mkdir $dirdown/Tools
+	mkdir $dirdown/Wordlists
+	mkdir $dirdown/LinuxScripts
 fi
 
 cloneRepo(){
 cd $dirdown/Tools
+echo -e "${YELLOW}[+] Cloning Dirsearch [+]${END}"
 echo -e "${CYAN}"
-echo -e "[+] Cloning Dirsearch [+]"
 git clone https://github.com/maurosoria/dirsearch.git
-echo -e "[+] Cloning nmapAutomator [+]"
+echo -e "${END}"
+echo -e "${YELLOW}[+] Cloning nmapAutomator [+]${END}"
+echo -e "${CYAN}"
 git clone https://github.com/21y4d/nmapAutomator.git
-echo -e "[+] Cloning GTFONow [+]"
+echo -e "${END}"
+echo -e "${YELLOW}[+] Cloning GTFONow [+]${END}"
+echo -e "${CYAN}"
 git clone https://github.com/Frissi0n/GTFONow.git
-echo -e "[+] Cloning JWTool [+]"
+echo -e "${END}"
+echo -e "${YELLOW}[+] Cloning JWTool [+]${END}"
+echo -e "${CYAN}"
 git clone https://github.com/ticarpi/jwt_tool.git
 sudo pytho3 -m pip install termcolor cprint pycryptodomex requests
-cd ../Wordlists
-echo -e "[+] Cloning SecLists [+]"
-git clone https://github.com/danielmiessler/SecLists.git
-echo -e "[+] All repositories cloned [+]"
 echo -e "${END}"
+cd ../Wordlists
+echo -e "${YELLOW}[+] Cloning SecLists [+]${END}"
+echo -e "${CYAN}"
+#git clone https://github.com/danielmiessler/SecLists.git
+echo -e "${END}"
+echo -e "${YELLOW}[+] All repositories cloned [+]${END}"
 echo -e "${PURPLE}[+] DONE [+] ${END}"
 }
 
 installTools(){
-echo -e "${CYAN} [...] Installing other tools [...] ${END}"
+echo -e "${YELLOW} [...] Installing other tools [...] ${END}"
+echo -e "${CYAN}"
 sudo apt-get install steghide -y
 sudo apt-get install fcrackzip -y
+echo -e "${END}"
 echo -e "${PURPLE}[+] DONE [+] ${END}"
+}
+
+installScripts(){
+cd ../LinuxScripts
+echo -e "${YELLOW} [...] Downloading Linux Scripts [...] ${END}"
+echo -e "${CYAN}"
+sudo wget https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh
+sudo wget https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh
+sudo wget https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh
+echo -e "${END}"
 }
 
 cloneRepo
 installTools
+installScripts
 
 echo -e "${RED}[+] EXITING... [+] ${END}"
